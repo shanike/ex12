1c3e87697916618ae37405ae08759a9496b90303
ayala <ayala.kvo@gmail.com>
2021-06-28 15:23:15 +0300

    llitel changes in timer

--- timer.py
+++ timer.py
@@ -6,14 +6,11 @@ class Timer:
     def __init__(self, parent, count):
         self.parent = parent
         self.count = count
-        self.minute = StringVar()
-        self.second = StringVar()
+        # setting the default value as 0
+        self.minute = StringVar("03")
+        self.second = StringVar("00")
 
     def set_timer(self):
-        # setting the default value as 0
-        self.minute.set("03")
-        self.second.set("00")
-
         # set labels
         minute_label = Label(self.parent, width=3, font=("Arial", 18, ""),
                              textvariable=self.minute, bg="#ffd6ba", foreground="white")
@@ -36,10 +33,6 @@ class Timer:
             self.minute.set("{0:0=2d}".format(mins))
             self.second.set("{0:0=2d}".format(secs))
 
-            # updating the GUI window after decrementing the
-            # temp value every time
-            self.parent.update()
-
             # when temp value = 0; then a messagebox pop's up
             # with a message:"Time's up"
             if count == 0:
@@ -50,18 +43,3 @@ class Timer:
 
     def handle_end_game(self):
         pass
-
-# # creating Tk window
-# parent = Tk()
-#
-# # setting geometry of tk window
-# parent.geometry("300x250")
-#
-# # Using title() to display a message in
-# # the dialogue box of the message in the
-# # title bar.
-# parent.title("Time Counter")
-#
-# timer = Timer(parent, 180)
-#
-# parent.mainloop()
